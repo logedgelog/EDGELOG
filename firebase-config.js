70b4fe658afc2b14e870ffba7c68970f94074df8
logedgelog <log.edgelog@gmail.com>
2026-06-17 15:53:59 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// ── EdgeLog Firebase Config ──
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyD3cbRKRxXx590FNw5FUr71avAetZ8ffLs",
+  authDomain:        "edgelog-368a4.firebaseapp.com",
+  projectId:         "edgelog-368a4",
+  storageBucket:     "edgelog-368a4.firebasestorage.app",
+  messagingSenderId: "46130170708",
+  appId:             "1:46130170708:web:752e0ee75814217aabd6c1",
+  measurementId:     "G-4RQZ3YSQQS"
+};
+
+const ADMIN_EMAIL = "log.edgelog@gmail.com";
